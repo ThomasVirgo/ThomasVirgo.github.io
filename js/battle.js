04b03cdb02c349387afe8c44b1bcea5db5ca0087
Thomas Virgo <tomcvirgo@gmail.com>
2021-05-12 11:46:34 +0100

    adding battle js script

---
+++ js/battle.js
@@ -0,0 +1,82 @@
+let speed = 5;
+let playerBullets = [];
+
+class Bullet{
+  constructor(xPos,yPos, xVel, yVel){
+    this.xPos=xPos;
+    this.yPos=yPos;
+    this.xVel=xVel;
+    this.yVel=yVel;
+  }
+}
+
+class Player{
+  constructor(x,y,size){
+    this.x = x;
+    this.y = y;
+    this.size = size;
+  }
+}
+
+let player1 = new Player(50,50,20)
+
+
+function setup() {
+  createCanvas(600, 600);
+}
+
+// draw loop
+function draw() {
+  background(220);
+  fill('red');
+  for (let i=0; i<playerBullets.length; i++){
+    let bullet = playerBullets[i];
+    updateBullet(bullet);
+    circle(bullet.xPos,bullet.yPos,5);
+  }
+  
+  rect(player1.x,player1.y,player1.size,player1.size);
+  if (keyIsDown(RIGHT_ARROW)){
+    updatePlayer('right');
+  } else if (keyIsDown(LEFT_ARROW)){
+    updatePlayer('left');
+  }
+  else if (keyIsDown(UP_ARROW)){
+    updatePlayer('up');
+  }
+  else if (keyIsDown(DOWN_ARROW)){
+    updatePlayer('down');
+  }
+  
+}
+
+function updatePlayer(move){
+  switch(move){
+    case 'right':
+      player1.x +=speed;
+      break;
+      
+    case 'left':
+      player1.x -=speed;
+      break;
+      
+    case 'up':
+      player1.y -=speed;
+      break;
+    
+    case 'down':
+      player1.y +=speed;
+      break   
+  }
+}
+
+function updateBullet(bullet){
+  bullet.xPos += bullet.xVel/100;
+  bullet.yPos += bullet.yVel/100;
+}
+  
+function mouseClicked(){
+  let gunDirection = createVector(mouseX-player1.x, mouseY-player1.y);
+  playerBullets.push(new Bullet(player1.x,player1.y,gunDirection.x, gunDirection.y));
+}
+  
