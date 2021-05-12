ba4651d1ac4333ab5c1a96dd9b00437a19fdcede
Thomas Virgo <tomcvirgo@gmail.com>
2021-05-12 17:48:39 +0100

    adding collision with obstacles

--- js/BattleGame/battle.js
+++ js/BattleGame/battle.js
@@ -1,19 +1,31 @@
-
+let player = new Player(50,380,20);
 let speed = 5;
 let gravity = 1;
 let playerBullets = [];
-let player1 = new Player(50,580,20);
-let isJumping = false;
+let obstacles = [new Obstacle(200,200,300,20), new Obstacle(100,100, 100, 20)];
+obstacles[0].distanceToPlayer(player);
 
+let isJumping = false;
+let dist1,dist2;
 
 function setup() {
-  let canvas = createCanvas(1000, 600);
+  let canvas = createCanvas(1000, 400);
   canvas.id('canvas');
+  dist1 = createP('1')
+  dist2 = createP('2')
 }
 
 // draw loop
 function draw() {
+  dist1.html(`Distance to obstacle 1: ${obstacles[0].distanceToPlayer(player)}`)
+  dist2.html(`Distance to obstacle 2: ${obstacles[1].distanceToPlayer(player)}`)
   background(220);
+  fill('black');
+  for (let i=0; i<obstacles.length; i++){
+    let ob = obstacles[i];
+    rect(ob.x, ob.y, ob.width, ob.height)
+  }
+  
   fill('red');
   for (let i=0; i<playerBullets.length; i++){
     let bullet = playerBullets[i];
@@ -21,13 +33,29 @@ function draw() {
     circle(bullet.xPos,bullet.yPos,5);
   }
   
-  rect(player1.x,player1.y,player1.size,player1.size);
-  player1.y -= player1.yVel;
-  player1.yVel -= gravity;
-  if (player1.y >= height-player1.size){
+  rect(player.x,player.y,player.size,player.size);
+  player.y -= player.yVel;
+  player.yVel -= gravity;
+  if (player.y >= height-player.size){
     isJumping=false;
-    player1.yVel = 0;
+    player.yVel = 0;
   }
+
+  //check for collisions with obstacles
+  for (let i=0; i<obstacles.length; i++){
+    let ob = obstacles[i];
+    let a = Math.abs((player.x + player.size) - ob.left);
+    let b = Math.abs((ob.right-player.x));
+    let c = Math.abs(player.y - ob.bottom);
+    let d = Math.abs(ob.top - (player.y+player.size));
+    let distances = [a,b,c,d];
+    let countCloseEdges = distances.filter(item => item<5).length;
+    if (countCloseEdges > 2){
+      console.log('collision detected');
+    }
+    
+  }
+
   if (keyIsDown(RIGHT_ARROW)){
     updatePlayer('right');
   } else if (keyIsDown(LEFT_ARROW)){
@@ -38,21 +66,21 @@ function draw() {
 function keyPressed(){
   if (keyCode === UP_ARROW && !isJumping){
     isJumping = true;
-    player1.yVel = 20;
+    player.yVel = 20;
   }
 }
 
 function updatePlayer(move){
   switch(move){
     case 'right':
-      if (player1.x < width-player1.size){
-        player1.x +=speed;
+      if (player.x < width-player.size){
+        player.x +=speed;
       }
       break;
       
     case 'left':
-      if (player1.x > 0){
-        player1.x -=speed;
+      if (player.x > 0){
+        player.x -=speed;
       }
       break;  
   }
@@ -64,7 +92,7 @@ function updateBullet(bullet){
 }
   
 function mouseClicked(){
-  let gunDirection = createVector(mouseX-player1.x, mouseY-player1.y);
-  playerBullets.push(new Bullet(player1.x,player1.y,gunDirection.x, gunDirection.y));
+  let gunDirection = createVector(mouseX-player.x, mouseY-player.y);
+  playerBullets.push(new Bullet(player.x,player.y,gunDirection.x, gunDirection.y));
 }
   
--- js/BattleGame/classes.js
+++ js/BattleGame/classes.js
@@ -17,6 +17,49 @@ class Player{
     }
 }
 
+class Obstacle{
+    constructor(x,y,width,height){
+        this.x=x;
+        this.y=y;
+        this.width=width;
+        this.height=height;
+        this.left = this.x;
+        this.right = this.x + this.width;
+        this.top = this.y;
+        this.bottom = this.y + this.height;
+    }
+
+    playerInXRange(player){
+        if ((player.x+player.size) > this.left || player.x < this.right){
+            return true
+        }
+        return false
+    }
+
+    playerInYRange(player){
+        if ((player.y+player.size) > this.top || player.y < this.bottom){
+            return true
+        }
+        return false
+    }
+
+    distanceToPlayer = (player) =>{
+        if (this.playerInXRange(player)){
+            let belowDist = Math.abs(player.y - this.bottom);
+            let aboveDist = Math.abs((this.top - (player.y+player.size)))
+            return Math.min(belowDist, aboveDist);
+        }
+        if (this.playerInYRange(player)){
+            let rightDist = Math.abs(player.x - this.right);
+            let leftDist = Math.abs((this.left - (player.x+player.size)))
+            return Math.min(leftDist, rightDist);
+        }
+        //otherwise not within obstacle co-ords
+    }
+
+
+}
+
 class Weapon{
     constructor(fireRate, bulletSize, ammo){
         this.fireRate = fireRate;
